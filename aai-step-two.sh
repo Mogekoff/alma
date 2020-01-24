d3e75095c0ce0249d312c9d469d383c17e6a43fc
Adrian <mogekoff@protonmail.ch>
2020-01-24 17:31:18 +0500

    Update aai-step-two.sh

--- aai-step-two.sh
+++ aai-step-two.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo en_US_UTF-8 UTF-8 > /etc/locale.gen
-echo ru_RU_UTF-8 UTF-8 >> /etc/locale.gen
+echo en_US.UTF-8 UTF-8 > /etc/locale.gen
+echo ru_RU.UTF-8 UTF-8 >> /etc/locale.gen
 locale-gen
 echo LANG=ru_RU.UTF-8 > /etc/locale.conf
 export LANG=ru_RU.UTF-8
@@ -11,7 +11,7 @@ echo usernamecomputer > /etc/hostname
 passwd root
 useradd -m -g users -G wheel -s /bin/bash username
 passwd username
-echo username ALL=(ALL) ALL >> /etc/sudoers
+echo "username ALL=(ALL) ALL" >> /etc/sudoers
 
 pacman -S dhcpcd xorg xorg-xinit i3-wm ttf-freefont ttf-dejavu rxvt-unicode zsh alsa-tuils gdb git chromium qtcreator dmenu openssh htop nnn neofetch --noconfirm
 
