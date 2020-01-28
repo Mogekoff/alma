ec40c8d97da4079982f482c84bbfec06c15ab8d7
Adrian <mogekoff@protonmail.ch>
2020-01-28 23:27:06 +0500

    Update aai-step-three.sh

--- aai-step-three.sh
+++ aai-step-three.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 #CHECK THE SCRIPT IS NOT BEING RUN BY ROOT
+
 if [ "$(id -u)" == "0" ]; then
    echo "This script must not be run as root"
    exit 1
 fi 
 
 #MINIMAL ARCHER LIST
-minimal="i3 rxvt-unicode compton conky dmenu zsh nano nnn feh moc mpv htop git openssh neofetch surf lynx"
+minimal="i3 rxvt-unicode picom conky dmenu zsh nano nnn feh moc mpv htop git openssh neofetch surf lynx"
 
 #PACKAGES FOR PROGRAMMERS
 prog="code qtcreator gdb"
@@ -21,6 +22,7 @@ browsers="chromium firefox"
 #SOME UTILS
 utils="xf86-input-synaptics"
 
+#INSTALLING PACKAGES
 
 sudo pacman -S $minimal --noconfirm
 sudo pacman -S $prog --noconfirm
@@ -30,34 +32,84 @@ sudo pacman -S $utils --noconfirm
 
 
 #CREATING BUILD DIR
+
 mkdir build
 cd build
 
 #JS CLI BROWSER
+
 git clone https://aur.archlinux.org/browsh.git
 cd browsh
-makepkg -sri
+makepkg -sri --noconfirm
 cd ..
 
 #NCURSES DISPLAY MANAGER
+
 git clone https://aur.archlinux.org/ly-git.git
 cd ly-git
-makepkg -sri
+makepkg -sri --noconfirm
 cd ..
+sudo systemctl enable ly
+sudo systemctl disable getty@tty2
 
 #OH-MY-ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 #CHANGING DEFAULT SHELL FOR ROOT AND USER
+
 chsh -s /bin/zsh
 sudo chsh root -s /bin/zsh
 
+#URXVT CONFIG
+
+touch ~/.Xresources
+echo -e "
+URxvt.scrollBar: false
+URxvt*scrollTtyKeypress: true
+URxvt*font: xft:DejaVu Sans Mono:size=12
+URxvt.letterSpace: -1
+*.foreground:   #cbcbd8
+*.background:   #0e0d18
+*.cursorColor:  #cbcbd8
+*.color0:       #171625
+*.color8:       #e1a8cc
+*.color1:       #1f1f31
+*.color9:       #79c69e
+*.color2:       #302f48
+*.color10:      #bebde9
+*.color3:       #474668
+*.color11:      #f09472
+*.color4:       #646387
+*.color12:      #e56ec1
+*.color5:       #8887a5
+*.color13:      #9794d8
+*.color6:       #b2b2c5
+*.color14:      #bcbe5f
+*.color7:       #e2e2e9
+*.color15:      #64b1bd
+urxvt*depth: 32
+urxvt*background: rgba:0000/0000/0200/c800
+" > ~/.Xresources
+
+#AUTOSTART
+
+touch ~/.xinitrc
+echo -e "
+xrdb -merge ~/.Xresources &
+picom & &
+feh --bg-scale ~/wallpaper
+" > ~/.xinitrc
+
+
 #SETTING UP "TERMINALPARTY" THEME FOR ZSH
-echo "export ZSH=""$USER"/.oh-my.zsh" >  ~/.zshrc
-echo "ZSH_THEME="terminalparty"" >> ~/.zshrc
+
+echo -e "
+export ZSH=$HOME/.oh-my.zsh
+ZSH_THEME="terminalparty"
+plugins=(git)
+source $ZSH/oh-my-zsh.sh" > ~/.zshrc
 
 #CLEARING BUILD DIR
 rm -r build/*
 
-#LAST REBOOT
-reboot
+exit 1
