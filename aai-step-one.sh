e23a5ae2a65a6dddd25e09a693684e9074e3a76a
Adrian <mogekoff@protonmail.ch>
2020-01-28 20:26:44 +0500

    Update aai-step-one.sh

--- aai-step-one.sh
+++ aai-step-one.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+#BOOT PART 500MB
+
 (
  echo o;
  echo n;
  echo;
  echo;
  echo;
- echo +32M;
+ echo +500M;
  echo w;
 ) | fdisk /dev/sda
+
+#SWAP PART
 
 read -p "GiB for swap partition: " swap
 if [[ $swap > 0 ]]
@@ -28,6 +32,8 @@ then
  swapon /dev/sda2
 fi
 
+#ROOT PART
+
 (  
  echo n;
  echo;
@@ -35,19 +41,27 @@ fi
  echo;
  echo;
  echo y;
-  
  echo w;
-
 ) | fdisk /dev/sda
+
+#DISPLAY PARTITIONS
 fdisk -l
+
+#FORMAT
 
 mkfs.ext4 /dev/sda1                                          
 mkfs.ext4 /dev/sda3
 mount /dev/sda3 /mnt                            
 mkdir /mnt/boot /mnt/var /mnt/home
 mount /dev/sda1 /mnt/boot
+
+#BASE INSTALL
+
 pacstrap /mnt base base-devel linux linux-firmware
 pacstrap /mnt grub-bios
+
+#GENFSTAB
 genfstab -p /mnt >> /mnt/etc/fstab 
 
+#LOG-IN TO ARCH-CHROOT
 arch-chroot /mnt sh -c "$(curl -fsSL https://git.io/JvtHM)"
