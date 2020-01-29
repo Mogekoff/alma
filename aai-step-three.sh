91db553a4f72169da8beb5f0bdc73d38637c7d7c
Adrian <mogekoff@protonmail.ch>
2020-01-29 21:59:06 +0500

    Update aai-step-three.sh

--- aai-step-three.sh
+++ aai-step-three.sh
@@ -7,6 +7,8 @@ if [[ "$(id -u)" == "0" ]]; then
    exit 1
 fi 
 
+cd
+
 read -p "Do you want to execute step three and install recommended soft and configs? (y/n): " choice
 if [[ choice == n ]]; then; exit 2; fi
 
